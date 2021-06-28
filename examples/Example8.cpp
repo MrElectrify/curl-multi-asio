98d81d41e0663f3a1a55b9ab289dcfbc05bcd181
andrewbuck86 <ajbuck@crimson.ua.edu>
2021-06-28 16:40:08 -0400

    Add example8 comment

--- examples/Example8.cpp
+++ examples/Example8.cpp
@@ -16,8 +16,17 @@ int main()
 	// setting each option, and fail if those don't work. for
 	// simplicity's sake we won't do that here
 	cma::Easy easy;
+	// there's an overload for SetURL that accepts key-value pairs
+	// of url=encoded parameters. this is simply a convenience to
+	// make some code look neater and more understandable when
+	// working with url-encoded parameters
 	easy.SetURL("http://www.postman-echo.com/get", 
 		{ { "key", "value" }, { "another", "keyvalue" } });
+	// this also accepts key-value pairs, and sets custom headers.
+	// if we want to clear custom headers, simply use ClearHeaders.
+	// the lifetime of the headers is managed by the easy handle
+	// and will be automatically freed otherwise once the handle
+	// goes out of scope
 	easy.AddHeader({ "cma", "true" });
 	easy.AddHeader({ "another", "header" });
 	// this is where the real magic happens. the request is
